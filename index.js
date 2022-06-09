72f4a201f7699534d41083c0c3a0d6165d43687b
Jake Burton <jburto94@yahoo.com>
2022-06-09 16:33:10 -0400

    backend now deletes entries on database

--- index.js
+++ index.js
@@ -53,10 +53,8 @@ app.post('/api/persons', (req, res) => {
 });
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(person => person.id !== id);
-
-  res.status(204).end();
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => res.status(204).end());
 });
 
 app.get('/info', (req, res) => {
